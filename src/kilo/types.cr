2bfeb9c8145ee5c48b8b2b64ea24b791dcf70889
Tarek Ghaleb <tghaleb@users.noreply.github.com>
2021-12-20 11:52:56 +0000

    added SideType and changes in SimpleScroesType

--- src/kilo/types.cr
+++ src/kilo/types.cr
@@ -11,17 +11,20 @@ module Kilo
     k32: String,
     name: String)
 
-  alias SimpleScoresType = Hash(Symbol, Int16 | Array(UInt8) | String)
+  alias SideType = Array(UInt8)
+  # alias SimpleScoresType = Hash(Symbol, Int16 | SideType | String)
+  alias SimpleScoresType = Hash(Symbol, Int16)
 
   NULL_SIMPLE_SCORES = SimpleScoresType{
-    :outward     => 0.to_i16,
-    :jumps       => 0.to_i16,
-    :hand        => 0.to_i16,
-    :hand_im     => 0.to_i16,
-    :same_both   => 0.to_i16,
-    :same_rp     => 0.to_i16,
-    :same_both_j => 0.to_i16,
-    :effort      => 0.to_i16,
+    :effort         => 0.to_i16,
+    :outward        => 0.to_i16,
+    :jumps          => 0.to_i16,
+    :hand           => 0.to_i16,
+    :hand_im        => 0.to_i16,
+    :same_finger_rp => 0.to_i16,
+    :same_finger_im => 0.to_i16,
+    :same_both      => 0.to_i16,
+    :same_both_j    => 0.to_i16,
   }
 
   alias KBWeightsConfType = Hash(Constants::Key, Int32)
@@ -31,13 +34,19 @@ module Kilo
   alias XKBListType = Hash(String, String)
   alias SideTo32Type = Array(Int32)
 
-  #  alias ImproveConfigType = Hash(String, Int16)
   alias ImproveConfigType = NamedTuple(
     stage2_count: Int16,
     effort_delta: Int16,
     filter_factor: Float64,
-    # use to filter stage 1
-    max_half_same_rp: Int16,
-    max_half_same_both: Int16,
+
+    # used for SQL WHERE
+    sql_half_same_finger_rp: Int16,
+    sql_half_same_finger_im: Int16,
+    sql_half_outward: Int16,
+    sql_half_jumps: Int16,
+
+    # used to filter stage 1
+    max_half_same_finger_rp: Int16,
+    max_half_same_finger_im: Int16,
     max_half_hand: Int16)
 end
